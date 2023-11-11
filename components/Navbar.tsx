8d782c5ec1a07fc8fc62919e16c3d6bff1ec155b
Ronin-Chan <yuhaochen37@outlook.com>
2023-11-11 22:51:31 +1100

    feat: upload video

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,10 +5,12 @@ import logo from '../utils/tiktik-logo.png'
 import { GoogleLogin, googleLogout } from '@react-oauth/google'
 import { createOrGetUser } from '../utils'
 import useAuthStore from '../store/authStore'
+import { IoMdAdd } from 'react-icons/io'
+import { AiOutlineLogout } from 'react-icons/ai'
 
 const Navbar = () => {
 
-  const { userProfile, addUser } = useAuthStore();
+  const { userProfile, addUser, removeUser } = useAuthStore();
 
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
@@ -26,11 +28,38 @@ const Navbar = () => {
       </div>
       <div>
         {userProfile ? (
-          <div>
-            {userProfile.userName}
+          <div className='flex gap-5 md:gap-10'>
+            <Link href="/upload">
+              <button className='flex border-2 px-2 md:px-4 text-md font-semibold items-center gap-2'>
+                <IoMdAdd className='text-xl' /> {` `}
+                <span className='hidden md:block'>Upload</span>
+              </button>
+            </Link>
+            {userProfile.image && (
+              <Link href="/">
+                <>
+                  <Image
+                    width={40}
+                    height={40}
+                    className='rounded-full cursor-pointer'
+                    src={userProfile.image}
+                    alt='profile photo'
+                  />
+                </>
+              </Link>
+            )}
+            <button
+              className='px-2'
+              onClick={() => {
+                googleLogout();
+                removeUser();
+              }}
+            >
+              <AiOutlineLogout color='purple' fontSize={23} />
+            </button>
           </div>
         ) : (
-          <GoogleLogin 
+          <GoogleLogin
             onSuccess={(res) => createOrGetUser(res, addUser)}
             onError={() => console.log('Error')}
           />
